17e2238976d37b3cfed0eba6b64ee58a35876869
Ataki_pw11 <Ataki_pw11@STHQ012B-07>
2022-12-12 16:39:26 +0400

    Search completed

--- Restaurant project/Source.cpp
+++ Restaurant project/Source.cpp
@@ -789,65 +789,168 @@ void AdminMenu(Kitchen& k, Stock& s) {
 
 void ClientMenu(double totalPrice, Client c, Kitchen k, Stock stock) {
 	c.ShowMenu();
-	cout << "Enter your choice to view : ";
-	int select;
-	cin >> select;
-	cout << endl;
-	k.ShowMealById(select);
-	cout << "[1] Order" << endl;
-	cout << "[2] Add ingredients" << endl;
-	cout << "Enter select : ";
-	int select1;
-	cin >> select1;
-	if (select1 == 1) {
-		cout << "Enter meal count : ";
-		int count;
-		cin >> count;
-		if (count > 0) {
-			auto meal = k.GetMealById(select);
-			totalPrice += count * meal->GetPrice();
-			cout << "Total price : " << totalPrice << " $" << endl;
-			cout << "[1] Continue" << endl;
-			cout << "[2] Finish" << endl;
-			cout << "Select : ";
-			int select;
-			cin >> select;
-			if (select == 1) {
-				ClientMenu(totalPrice, c, k, stock);
+	cout << "[1] Select meal" << endl;
+	cout << "[2] Search meal" << endl;
+	cout << "[3] Notification" << endl;
+	cout << "Enter your select : ";
+	int choice;
+	cin >> choice;
+	if (choice == 1) {
+		cout << "Enter your choice to view : ";
+		int select;
+		cin >> select;
+		cout << endl;
+		k.ShowMealById(select);
+		cout << "[1] Order" << endl;
+		cout << "[2] Add ingredients" << endl;
+		cout << "Enter select : ";
+		int select1;
+		cin >> select1;
+		if (select1 == 1) {
+			cout << "Enter meal count : ";
+			int count;
+			cin >> count;
+			if (count > 0) {
+				auto meal = k.GetMealById(select);
+				totalPrice += count * meal->GetPrice();
+				cout << "Total price : " << totalPrice << " $" << endl;
+				cout << "[1] Continue" << endl;
+				cout << "[2] Finish" << endl;
+				cout << "Select : ";
+				int select;
+				cin >> select;
+				if (select == 1) {
+					ClientMenu(totalPrice, c, k, stock);
+				}
+				else if (select == 2) {
+
+
+				}
 			}
-			else if (select == 2) {
-				
+		}
+		else if (select1 == 2) {
+			cout << endl;
+			stock.Show();
+			cout << "Enter ingredients id : ";
+			int id;
+			cin >> id;
+			auto product = stock.GetProductById(id);
+			cout << *product << endl;
+			cout << "Enter " << product->GetName() << " count : ";
+			double count;
+			cin >> count;
+			if (stock.ProductCount(id, count)) {
+				auto meal = k.GetMealById(select);
+				meal->AddProduct(*product);
+				totalPrice += meal->GetPrice();
+				double a = product->GetPrice();
+				totalPrice += product->GetPrice() * count;
+				cout << "\n" << *meal << endl;
+				cout << "Ingredients added successfully" << endl << endl;
+				cout << "Total price : " << totalPrice << " $" << endl;
+			}
+			else {
+				auto meal = k.GetMealById(select);
+				totalPrice += meal->GetPrice();
+				cout << "Ingredients out of stock" << endl;
+			}
+		}
+	}
+	else if (choice == 2) {
+		cout << "Enter meal name : ";
+		int a1;
+		int num = 0;
+		for (size_t i = 0; i < 6; i++)
+		{
+			char meal = _getch();
+			cout << meal;
+			int b = k.GetMealCount();
+			for (size_t i3 = 0; i3 < k.GetMealCount(); i3++)
+			{
+				Meal meal1 = k.meals[i3];
+				cout << endl;
+				int i2 = 0;
+				while (true)
+				{
+					char a = meal1.GetName()[i2];
+					if (meal1.GetName()[i2] == meal) {
+						string a = meal1.GetName();
+						cout << "[" << ++num << "] " << a << endl;
+					}
+					++i2;
+					if (meal1.GetName().length() == i2) {
+						a1 = i3;
+						break;
+					}
+				}
+			}
+			if (char(meal) >= 48 && char(meal) <= 57) {
+				break;
+			}
+			else {
 
 			}
 		}
-	}
-	else if (select1 == 2) {
-		cout << endl;
-		stock.Show();
-		cout << "Enter ingredients id : ";
-		int id;
-		cin >> id;
-		auto product = stock.GetProductById(id);
-		cout << *product << endl;
-		cout << "Enter " << product->GetName() << " count : ";
-		double count;
-		cin >> count;
-		if (stock.ProductCount(id, count)) {
-			auto meal = k.GetMealById(select);
-			meal->AddProduct(*product);
-			totalPrice += meal->GetPrice();
-			double a = product->GetPrice();
-			totalPrice += product->GetPrice() * count;
-			cout << "\n" << *meal << endl;
-			cout << "Ingredients added successfully" << endl << endl;
-			cout << "Total price : " << totalPrice << " $" << endl;
+		k.ShowMealById(a1);
+		cout << "[1] Order" << endl;
+		cout << "[2] Add ingredients" << endl;
+		cout << "Enter select : ";
+		int select1;
+		cin >> select1;
+		if (select1 == 1) {
+			cout << "Enter meal count : ";
+			int count;
+			cin >> count;
+			if (count > 0) {
+				auto meal = k.GetMealById(a1);
+				totalPrice += count * meal->GetPrice();
+				cout << "Total price : " << totalPrice << " $" << endl;
+				cout << "[1] Continue" << endl;
+				cout << "[2] Finish" << endl;
+				cout << "Select : ";
+				int select;
+				cin >> select;
+				if (select == 1) {
+					ClientMenu(totalPrice, c, k, stock);
+				}
+				else if (select == 2) {
+
+
+				}
+			}
 		}
-		else {
-			auto meal = k.GetMealById(select);
-			totalPrice += meal->GetPrice();
-			cout << "Ingredients out of stock" << endl;
+		else if (select1 == 2) {
+			cout << endl;
+			stock.Show();
+			cout << "Enter ingredients id : ";
+			int id;
+			cin >> id;
+			auto product = stock.GetProductById(id);
+			cout << *product << endl;
+			cout << "Enter " << product->GetName() << " count : ";
+			double count;
+			cin >> count;
+			if (stock.ProductCount(id, count)) {
+				auto meal = k.GetMealById(a1);
+				meal->AddProduct(*product);
+				totalPrice += meal->GetPrice();
+				double a = product->GetPrice();
+				totalPrice += product->GetPrice() * count;
+				cout << "\n" << *meal << endl;
+				cout << "Ingredients added successfully" << endl << endl;
+				cout << "Total price : " << totalPrice << " $" << endl;
+			}
+			else {
+				auto meal = k.GetMealById(a1);
+				totalPrice += meal->GetPrice();
+				cout << "Ingredients out of stock" << endl;
+			}
 		}
+
+
+
 	}
+
 };
 
 void SendNotificationToClient(ClientNotification& n, Table& t) {
@@ -872,12 +975,12 @@ void main() {
 	stocksave.AddProduct(p2);
 	stocksave.AddProduct(p3);
 	stocksave.AddProduct(p4);
-	Meal f(20, "Mozerella");
+	Meal f(20, "Moz");
 	f.AddProduct(p);
 	f.AddProduct(p1);
 	f.AddProduct(p2);
 	f.AddProduct(p3);
-	Meal f1(13, "Sezar");
+	Meal f1(13, "SeMzar");
 	f1.AddProduct(p);
 	f1.AddProduct(p1);
 	Kitchen k;
